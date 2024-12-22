51c0c398d187349c1e9d4bbcfe55443a7591ffa2
Karrenbelt <karrenbelt_git@pm.me>
2024-12-22 23:06:50 +0100

    feat: initial map_components implementation for Nabla

--- substreams/ethereum-nabla/src/modules.rs
+++ substreams/ethereum-nabla/src/modules.rs
@@ -1,11 +1,26 @@
+use anyhow::Result;
 use substreams_ethereum::pb::eth;
-use tycho_substreams::prelude::*;
+use tycho_substreams::prelude::{
+    BlockTransactionProtocolComponents, TransactionProtocolComponents,
+};
 
 #[substreams::handlers::map]
-fn map_protocol_changes(
-    block: eth::v2::Block,
-) -> Result<BlockContractChanges, substreams::errors::Error> {
-    let mut transaction_contract_changes = Vec::<TransactionContractChanges>::new();
-    // TODO: protocol specific logic goes here
-    Ok(BlockContractChanges { block: Some((&block).into()), changes: transaction_contract_changes })
+pub fn map_components(block: eth::v2::Block) -> Result<BlockTransactionProtocolComponents> {
+    Ok(BlockTransactionProtocolComponents {
+        tx_components: block
+            .transactions()
+            .filter_map(|tx| {
+                let components = tx
+                    .logs_with_calls()
+                    .filter_map(|(log, _call)| {
+                        println!("Log data: {:?}", log.data);
+                        None
+                    })
+                    .collect::<Vec<_>>();
+
+                (!components.is_empty())
+                    .then(|| TransactionProtocolComponents { tx: Some(tx.into()), components })
+            })
+            .collect(),
+    })
 }
