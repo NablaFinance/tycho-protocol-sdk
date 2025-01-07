a04d80b7c4f736e4cb2817ee27cf535db9963e72
zarathustra <zarathustra_git@pm.me>
2025-01-08 00:17:17 +0100

    test: Nabla test_map_component draft

--- substreams/ethereum-nabla/src/modules.rs
+++ substreams/ethereum-nabla/src/modules.rs
@@ -12,8 +12,8 @@ pub fn map_components(block: eth::v2::Block) -> Result<BlockTransactionProtocolC
             .filter_map(|tx| {
                 let components = tx
                     .logs_with_calls()
-                    .filter_map(|(log, _call)| {
-                        println!("Log data: {:?}", log.data);
+                    .filter_map(|(_log, _call)| {
+                        // println!("Log data: {:?}", log.data);
                         None
                     })
                     .collect::<Vec<_>>();
@@ -23,4 +23,40 @@ pub fn map_components(block: eth::v2::Block) -> Result<BlockTransactionProtocolC
             })
             .collect(),
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::proto::sf::bstream::v1::Block;
+
+    use prost::Message;
+
+    use std::fs::File;
+    use std::io::Read;
+
+    #[test]
+    fn test_map_components() {
+        let file_path = "../../../firehose-data/storage/merged-blocks/0266195200.bin";
+        let mut file = File::open(file_path).expect("Failed to open merged-blocks file");
+        let mut block_data = Vec::new();
+        file.read_to_end(&mut block_data)
+            .expect("Failed to read merged-blocks file");
+
+        let block = Block::decode(&*block_data).expect("Failed to decode");
+
+        if let Some(payload) = block.payload {
+            // Confirm the type_url matches the expected type
+            if payload.type_url == "type.googleapis.com/sf.ethereum.type.v2.Block" {
+                // Decode the raw `value` field into an Ethereum block
+                let eth_block = eth::v2::Block::decode(&*payload.value)
+                    .expect("Failed to decode Ethereum block");
+                println!("Decoded Ethereum Block: {:?}", eth_block);
+            } else {
+                println!("Unexpected payload type_url: {}", payload.type_url);
+            }
+        } else {
+            println!("No payload found in the block");
+        }
+    }
 }
