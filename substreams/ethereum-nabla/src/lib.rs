9d32cb90eb4a5b442da83297155722ba04fae10e
zarathustra <zarathustra_git@pm.me>
2025-01-13 22:54:07 +0100

    feat: BlockProcessor

--- substreams/ethereum-nabla/src/lib.rs
+++ substreams/ethereum-nabla/src/lib.rs
@@ -1,4 +1,5 @@
 mod abi;
+mod block_reader;
 mod modules;
 mod pb;
 mod proto;
--- substreams/ethereum-nabla/src/modules.rs
+++ substreams/ethereum-nabla/src/modules.rs
@@ -1,19 +1,56 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+use crate::abi;
 use anyhow::Result;
-use substreams_ethereum::pb::eth;
+use substreams::hex;
+use substreams_ethereum::{pb::eth, Event};
 use tycho_substreams::prelude::{
-    BlockTransactionProtocolComponents, TransactionProtocolComponents,
+    BlockTransactionProtocolComponents, ProtocolComponent, TransactionProtocolComponents,
 };
 
-#[substreams::handlers::map]
+pub const PORTAL_CONTRACT: [u8; 20] = hex!("cB94Eee869a2041F3B44da423F78134aFb6b676B");
+
+fn is_deployment_tx(tx: &eth::v2::TransactionTrace, vault_address: &[u8]) -> bool {
+    let created_accounts = tx
+        .calls
+        .iter()
+        .flat_map(|call| {
+            call.account_creations
+                .iter()
+                .map(|ac| ac.account.to_owned())
+        })
+        .collect::<Vec<_>>();
+
+    if let Some(deployed_address) = created_accounts.first() {
+        return deployed_address.as_slice() == vault_address;
+    }
+    false
+}
+
+// #[substreams::handlers::map]
 pub fn map_components(block: eth::v2::Block) -> Result<BlockTransactionProtocolComponents> {
     Ok(BlockTransactionProtocolComponents {
         tx_components: block
             .transactions()
             .filter_map(|tx| {
+                if is_deployment_tx(tx, &PORTAL_CONTRACT) {
+                    println!("Block number: {:?}", block.number);
+                    println!("CONTRACT FOUND: {:?}", PORTAL_CONTRACT);
+                }
+                // detect the registered asset events for portal
                 let components = tx
                     .logs_with_calls()
-                    .filter_map(|(_log, _call)| {
-                        // println!("Log data: {:?}", log.data);
+                    .filter_map(|(log, call)| {
+                        let address = log.address.as_slice();
+                        if address == PORTAL_CONTRACT {
+                            println!("Block number: {:?}", block.number);
+                            println!("{:?}", log);
+                            let asset_registered =
+                                abi::nabla_portal::events::AssetRegistered::match_and_decode(log)?;
+                            println!("Registered Asset: {:?}", asset_registered);
+                        }
+
                         None
                     })
                     .collect::<Vec<_>>();
@@ -29,34 +66,24 @@ pub fn map_components(block: eth::v2::Block) -> Result<BlockTransactionProtocolC
 mod tests {
     use super::*;
     use crate::proto::sf::bstream::v1::Block;
+    use crate::{abi, block_reader};
 
     use prost::Message;
-
     use std::fs::File;
     use std::io::Read;
+    use std::path::PathBuf;
 
     #[test]
     fn test_map_components() {
-        let file_path = "../../../firehose-data/storage/merged-blocks/0266195200.bin";
-        let mut file = File::open(file_path).expect("Failed to open merged-blocks file");
-        let mut block_data = Vec::new();
-        file.read_to_end(&mut block_data)
-            .expect("Failed to read merged-blocks file");
+        let merged_dir = "../../../firehose-data/storage/merged-blocks";
+        let block_processor = block_reader::BlockProcessor::new(merged_dir)
+            .expect("Failed to initialize BlockProcessor");
 
-        let block = Block::decode(&*block_data).expect("Failed to decode");
-
-        if let Some(payload) = block.payload {
-            // Confirm the type_url matches the expected type
-            if payload.type_url == "type.googleapis.com/sf.ethereum.type.v2.Block" {
-                // Decode the raw `value` field into an Ethereum block
-                let eth_block = eth::v2::Block::decode(&*payload.value)
-                    .expect("Failed to decode Ethereum block");
-                println!("Decoded Ethereum Block: {:?}", eth_block);
-            } else {
-                println!("Unexpected payload type_url: {}", payload.type_url);
+        for block in block_processor.take(10_000) {
+            if block.number % 1_000 == 0 {
+                println!("block: {:?}", block.number);
             }
-        } else {
-            println!("No payload found in the block");
+            let _ = map_components(block);
         }
     }
 }
