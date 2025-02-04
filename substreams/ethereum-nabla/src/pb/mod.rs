43e72f00cd061a0dc4726c7a9cd9aea476360c1c
zarathustra <zarathustra_git@pm.me>
2025-02-04 17:21:15 +0100

    feat: component type .proto definitions for storage

--- substreams/ethereum-nabla/src/pb/mod.rs
+++ substreams/ethereum-nabla/src/pb/mod.rs
@@ -1,4 +1,11 @@
 // @generated
+pub mod nabla {
+    // @@protoc_insertion_point(attribute:nabla.v1)
+    pub mod v1 {
+        include!("nabla.v1.rs");
+        // @@protoc_insertion_point(nabla.v1)
+    }
+}
 pub mod tycho {
     pub mod evm {
         // @@protoc_insertion_point(attribute:tycho.evm.v1)
