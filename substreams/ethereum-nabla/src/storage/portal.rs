ec06ddba43cfad093007a56c85408b29e69b8a1c
zarathustra <zarathustra_git@pm.me>
2025-02-06 18:31:10 +0100

    feat: StorageType for StorageLocation

--- substreams/ethereum-nabla/src/storage/portal.rs
+++ substreams/ethereum-nabla/src/storage/portal.rs
@@ -1,53 +1,51 @@
-#![allow(dead_code)]
-
-use crate::storage::utils::StorageLocation;
+use crate::storage::utils::{StorageLocation, StorageType};
 use substreams::hex;
 
 pub const OWNER: StorageLocation = StorageLocation {
     name: "owner",
+    storage_type: StorageType::Address,
     slot: hex!("0000000000000000000000000000000000000000000000000000000000000000"),
     offset: 0,
-    number_of_bytes: 20,
 };
 
 pub const PAUSED: StorageLocation = StorageLocation {
     name: "paused",
+    storage_type: StorageType::Bool,
     slot: hex!("0000000000000000000000000000000000000000000000000000000000000000"),
     offset: 20,
-    number_of_bytes: 1,
 };
 
 pub const GATE: StorageLocation = StorageLocation {
     name: "gate",
+    storage_type: StorageType::Address,
     slot: hex!("0000000000000000000000000000000000000000000000000000000000000002"),
     offset: 0,
-    number_of_bytes: 20,
 };
 
 pub const GATED: StorageLocation = StorageLocation {
     name: "gated",
+    storage_type: StorageType::Bool,
     slot: hex!("0000000000000000000000000000000000000000000000000000000000000002"),
     offset: 20,
-    number_of_bytes: 1,
 };
 
 pub const ORACLE_ADAPTER: StorageLocation = StorageLocation {
     name: "oracle_adapter",
+    storage_type: StorageType::Address,
     slot: hex!("0000000000000000000000000000000000000000000000000000000000000003"),
     offset: 0,
-    number_of_bytes: 20,
 };
 
 pub const GUARD_ORACLE: StorageLocation = StorageLocation {
     name: "oracle_adapter",
+    storage_type: StorageType::Address,
     slot: hex!("0000000000000000000000000000000000000000000000000000000000000004"),
     offset: 0,
-    number_of_bytes: 20,
 };
 
 pub const ROUTERS: StorageLocation = StorageLocation {
     name: "routers",
+    storage_type: StorageType::Array { item_type: &StorageType::Address },
     slot: hex!("0000000000000000000000000000000000000000000000000000000000000006"),
     offset: 0,
-    number_of_bytes: 32,
 };
--- substreams/ethereum-nabla/src/storage/utils.rs
+++ substreams/ethereum-nabla/src/storage/utils.rs
@@ -1,13 +1,53 @@
 #![allow(dead_code)]
-#![allow(unused_variables)]
-#![allow(unused_imports)]
+
+#[derive(Clone)]
+pub enum StorageType {
+    Address,
+    Bool,
+    Uint256,
+    Array { item_type: &'static StorageType },
+    Mapping { key_type: &'static StorageType, value_type: &'static StorageType },
+}
+
+impl StorageType {
+    pub fn number_of_bytes(&self) -> usize {
+        match self {
+            StorageType::Address => 20,
+            StorageType::Bool => 1,
+            StorageType::Uint256 => 32,
+            StorageType::Array { .. } => 32,
+            StorageType::Mapping { .. } => 32,
+        }
+    }
+    pub fn item_type(&self) -> Result<&StorageType, String> {
+        if let StorageType::Array { item_type } = self {
+            Ok(item_type)
+        } else {
+            Err("StorageType is not an Array".into())
+        }
+    }
+    pub fn key_type(&self) -> Result<&StorageType, String> {
+        if let StorageType::Mapping { key_type, .. } = self {
+            Ok(key_type)
+        } else {
+            Err("StorageType is not a Mapping".into())
+        }
+    }
+    pub fn value_type(&self) -> Result<&StorageType, String> {
+        if let StorageType::Mapping { value_type, .. } = self {
+            Ok(value_type)
+        } else {
+            Err("StorageType is not a Mapping".into())
+        }
+    }
+}
 
 #[derive(Clone)]
 pub struct StorageLocation<'a> {
     pub name: &'a str,
+    pub storage_type: StorageType,
     pub slot: [u8; 32],
     pub offset: usize,
-    pub number_of_bytes: usize,
 }
 
 pub fn read_bytes(buf: &[u8], offset: usize, number_of_bytes: usize) -> &[u8] {
